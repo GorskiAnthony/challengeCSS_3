b82c3b0d04cd085169b06393b4bb2257d5c26b05
Anthony Gorski <anthony.developpeurweb@gmail.com>
2020-05-13 23:05:01 +0200

    little optimisation (js file) :sweat_smile: Next step > Mobile view

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,31 +8,31 @@ const errorResponse = document.getElementsByClassName("error-response");
 
 const formCheck = [firstname, lastname, email, password];
 
-function error(propertie) {
+function error(propertie, id) {
  
 
     /* Test with Switch.. Doesn't work but is an idea */
     switch(propertie) {
 
         case 'firstname': 
-            console.log("error on firstname");
+            //console.log("error on firstname");
             firstname.classList.add('error');
-            errorResponse[0].innerHTML = "First Name cannot be empty"
+            errorResponse[id].innerHTML = "First Name cannot be empty"
             break;
         case 'lastname': 
-            console.log("error on lastname");
+            //console.log("error on lastname");
             lastname.classList.add('error');
-            errorResponse[1].innerHTML = "Last Name cannot be empty"
+            errorResponse[id].innerHTML = "Last Name cannot be empty"
             break;
         case 'email' :
-            console.log("error on email");
+            //console.log("error on email");
             email.classList.add('error');
-            errorResponse[2].innerHTML = "Looks loke this is not an email"
+            errorResponse[id].innerHTML = "Looks loke this is not an email"
             break;
         case 'password':
-            console.log("error on password");
+            //console.log("error on password");
             password.classList.add('error');
-            errorResponse[3].innerHTML = "Password cannot be empty"
+            errorResponse[id].innerHTML = "Password cannot be empty"
             break;
         default:
             console.log(`Sorry we are out of ${propertie}`)
@@ -53,7 +53,7 @@ function checkForm() {
 
     for(index = 0; index < formCheck.length; index++) {
         if(!isEmpty(formCheck[index].value)) {
-            error(formCheck[index].name)
+            error(formCheck[index].name, index)
         } else {
             formCheck[index].classList.remove('error');
             errorResponse[index].innerHTML = "";
