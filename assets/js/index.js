ab1ebe26ee0edf8867e25dffb523c105388bc5c8
Anthony Gorski <anthony.developpeurweb@gmail.com>
2020-05-14 12:14:32 +0200

    i finish test all input: Done > Now WIP Responsive !

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,21 @@
 /* get form */
-
 const firstname = document.getElementById("firstname");
 const lastname= document.getElementById("lastname");
 const email= document.getElementById("email");
 const password= document.getElementById("password");
+/* get error response */
 const errorResponse = document.getElementsByClassName("error-response");
 
+/* create regex for check email on form */
+const regex = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+
+/* add all input of form on new array */
 const formCheck = [firstname, lastname, email, password];
 
+/** Function error is here for push an error on dom */
 function error(propertie, id) {
  
-
-    /* Test with Switch.. Doesn't work but is an idea */
+    /* Test with Switch.. */
     switch(propertie) {
 
         case 'firstname': 
@@ -39,20 +43,32 @@ function error(propertie, id) {
     }
 }
 
-function isEmpty(id) {
+/** Function isEmpty is boolean if empty return false else true*/
+function isEmpty(id, value) {
 
-    if(id !== "") {
+    //console.log(id + " : " + regex.test(value));
+    
+    if(value !== "") {
+        /** If value different of empty i test id === email and check my regex */
+        if(id === "email") {
+            return regex.test(value);
+        }
         return true;
     } else {
         return false;
     }
 }
 
+
+/** I call the function from the index.html */
 function checkForm() {
+
+    /** allows not to refresh the page */
     event.preventDefault();
 
+    /** a for loop to test each entry in the form */
     for(index = 0; index < formCheck.length; index++) {
-        if(!isEmpty(formCheck[index].value)) {
+        if(!isEmpty(formCheck[index].name, formCheck[index].value)) {
             error(formCheck[index].name, index)
         } else {
             formCheck[index].classList.remove('error');
